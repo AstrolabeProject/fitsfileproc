f04486be514f17438b8afe8ea794a68ae4e05320
Tom Hicks <hickst@tohono.com>
2019-08-01 05:28:27 -0700

    Refactor FITS file read & get all fields. Add get all keys method. Outline TODO items.

--- src/main/groovy/edu/arizona/astrolabe/ffp/JwstProcessor.groovy
+++ src/main/groovy/edu/arizona/astrolabe/ffp/JwstProcessor.groovy
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.*
  *   This class implements JWST-specific FITS file processing methods.
  *
  *   Written by: Tom Hicks. 7/28/2019.
- *   Last Modified: Update for rename of aliases file and addition of field info file.
+ *   Last Modified: Refactor FITS file read & get all fields. Add get all keys method. Outline TODO items.
  */
 class JwstProcessor implements IFitsFileProcessor {
   static final Logger log = LogManager.getLogger(JwstProcessor.class.getName());
@@ -43,62 +43,70 @@ class JwstProcessor implements IFitsFileProcessor {
     // load the field info from a given file or a default resource path.
     fitsFields = loadFields(config.fieldsFile)
     if (DEBUG)                              // REMOVE LATER
-      fitsAliases.each { entry -> println("${entry.key}=${entry.value}") }
+      fitsFields.each { entry -> println("${entry.key}=${entry.value}") }
   }
 
 
   /** Process the single given file. */
   int processAFile (File aFile) {
     log.trace("(FitsFileProcessor.processAFile): aFile=${aFile}")
-    if (DEBUG)                              // REMOVE LATER
-      println("FILE: ${aFile.getName()}")
 
-    // make FITS object from given FITS file
-    Fits fits = null
-    if (aFile.getName().endsWith('.gz'))
-      fits = new Fits(new FileInputStream(aFile))
-    else
-      fits = new Fits(aFile)
+    Fits fits = readFitsFile(aFile)         // make FITS object from given FITS file
+    if (!fits)                              // if unable to open/read FITS file
+      return 0                              // then skip this file
 
-    try {
-      fits.read()                             // read the data into FITS object
+    def allKeys = getAllKeys(fits)
+    if (DEBUG) {                            // REMOVE LATER
+      println("ALL KEYS(${allKeys.size()}): ${allKeys}") // REMOVE LATER
     }
-    catch (Exception iox) {
-      def msg = "Invalid FITS Header encountered in file '${aFile.getAbsolutePath()}'. File processing skipped."
-      log.warn("(JwstProcessor.processAFile): $msg")
-      System.err.println("WARNING: $msg")
-      return 0
-    }
+
+    // TODO:
+    // for all keys read {
+    //   map key to ObsCore name
+    //   get datatype from ObsCore key in fields map
+    //   read value of datatype using original key
+    //   if value missing: try to get the default value
+    //   if value is computed: try dispatch to calculation routine
+    //   if value still missing?: fill in null?
+    // }
+    // NOTE: default for t_exptime given by Eiichi Egami 20190626: 1347
+    // NOTE: default for instrument_name = NIRCam + MODULE value
+    // NOTE: Ask Eiichi about o_ucd: what is being measured? photo.flux.density? others?
+
 
     // def fitsFields = getFitsFields(fits)
     // println("FITS FIELDS(${fitsFields.size()}): ${fitsFields}")
     // fitsFields.each { key, val -> println("${key}: ${val}") }
 
-    def allFields = getAllFields(fits)
-    if (DEBUG) {                            // REMOVE LATER
-      println("ALL FIELDS(${allFields.size()}): ${allFields}")
-      allFields.each { key, val -> println("${key}: ${val}") }
-    }
+    // def allFields = getAllFields(fits)
+    // if (DEBUG) {                            // REMOVE LATER
+    //   println("ALL FIELDS(${allFields.size()}): ${allFields}")
+    //   allFields.each { key, val -> println("${key}: ${val}") }
+    // }
 
     return 1
   }
 
 
+  /**
+   * Return a List of all non-comment (key/value pair) keywords
+   * in the header of the first HDU of the given FITS file.
+   */
+  List getAllKeys (Fits refFits) {
+    log.trace("(FitsFileProcessor.getAllKeys): refFits=${refFits}")
+    Header hdr = refFits.getHDU(0).getHeader()
+    return hdr.iterator().findAll{it.isKeyValuePair()}.collect{it.getKey()}
+  }
+
+  /**
+   * Return a Map of all non-comment (key/value pair) keywords and their values
+   * in the header of the first HDU of the given FITS file.
+   */
   def getAllFields (Fits refFits) {
     log.trace("(FitsFileProcessor.getAllFields): refFits=${refFits}")
-    Map hdrMap = [:]
-
     Header hdr = refFits.getHDU(0).getHeader()
-    def iter = hdr.iterator();
-    for(HeaderCard card=iter.next(); iter.hasNext(); card=iter.next()) {
-      if (card.isKeyValuePair()) {
-        def key = card.getKey()
-        def val = card.getValue()
-        hdrMap << [(key) : val]
-      }
-    }
-
-    return hdrMap
+    return hdr.iterator().findAll{it.isKeyValuePair()}.collectEntries {
+      [ (it.getKey()) : it.getValue() ] }
   }
 
 
@@ -203,6 +211,32 @@ class JwstProcessor implements IFitsFileProcessor {
       log.info("(JwstProcessor.loadFields): Read ${recCnt} field information records.")
 
     return fields
+  }
+
+
+  /**
+   * Open, read, and return a Fits object from the given File, which is assumed to be
+   * pointing to a valid, readable FITS file.
+   * Returns the new Fits object, or null if problems encountered.
+   */
+  private Fits readFitsFile (File aFile) {
+    Fits fits = null
+    if (aFile.getName().endsWith('.gz'))
+      fits = new Fits(new FileInputStream(aFile))
+    else
+      fits = new Fits(aFile)
+
+    try {
+      fits.read()                             // read the data into FITS object
+    }
+    catch (Exception iox) {
+      def msg = "Invalid FITS Header encountered in file '${aFile.getAbsolutePath()}'. File processing skipped."
+      log.warn("(JwstProcessor.readFitsFile): $msg")
+      System.err.println("WARNING: $msg")
+      return null                           // signal unable to open/read FITS file
+    }
+
+    return fits                             // everything OK: return Fits object
   }
 
 }
